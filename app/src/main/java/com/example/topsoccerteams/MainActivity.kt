c27daaf3c45055c99476e717d17560586883389e
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-17 09:17:17 +0200

    declaring arrays were added

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.widget.TextView
+import java.util.Arrays
 class MainActivity : AppCompatActivity() {
     //
     //
-    val teams = arrayOf<String>("Bayern Munchen" )
+    val teams = arrayOf<String>("Bayern Munchen", "Arsenal", "Paris Saint-Germain", "Barcelona", "Real Madrid", "Inter Milan", "Manchester City", "Borussia Dortmund", "Liverpool FC", "Sporting")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        //
+        val teamstxtView = findViewById<TextView>(R.id.teamstxtView)
+        //
+        teamstxtView.text = teams[0]
+        teams[0] = "Bayern Munich"
+        //
+        teamstxtView.text = Arrays.toString(teams)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
