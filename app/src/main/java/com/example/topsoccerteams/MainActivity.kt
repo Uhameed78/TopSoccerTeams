1618e2a7d05cb7f45fb255ef5bc85af6ee7877e7
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-17 23:41:33 +0200

    added functions and made short name function

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -6,17 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
-import java.util.Arrays
 import android.util.Log
 //
 fun logArrayValues(arr: Array<String>) {
-    Log.v("ArrayValues", Arrays.toString(arr))
+    Log.v("ArrayValues", arr.contentToString())
+}
+//
+fun getShortestString(arr: Array<String>): String {
+    if (arr.isEmpty()) return ""
+    var shortestElement = arr[0]
+    for (element in arr) {
+        if (element.length < shortestElement.length) {
+            shortestElement = element
+        }
+    }
+    return shortestElement
 }
 
 class MainActivity : AppCompatActivity() {
     //
     //
-    val teams = arrayOf<String>("Bayern Munchen", "Arsenal", "Paris Saint-Germain", "Barcelona", "Real Madrid", "Inter Milan", "Manchester City", "Borussia Dortmund", "Liverpool FC", "Sporting")
+    val teams = arrayOf("Bayern Munchen", "Arsenal", "Paris Saint-Germain", "Barcelona", "Real Madrid", "Inter Milan", "Manchester City", "Borussia Dortmund", "Liverpool FC", "Sporting")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +37,14 @@ class MainActivity : AppCompatActivity() {
         //
         logArrayValues(teams)
 
-        //
-        val teamstxtView = findViewById<TextView>(R.id.teamstxtView)
-        //
-        var teamsDisplay = ""
-        for (team in teams) {
-            teamsDisplay += "${team}\n"
-        }
+        val shortestName = getShortestString(teams)
+        Log.v("ShortestName", shortestName)
 
-        //
-        teamstxtView.text = teamsDisplay
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = getString(R.string.shortest_name_label, shortestName)
+
+        val teamstxtView = findViewById<TextView>(R.id.teamstxtView)
+        teamstxtView.text = teams.joinToString("\n")
 
 
 
